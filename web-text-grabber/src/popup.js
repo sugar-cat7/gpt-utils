22e487f55a49e521768831c663ea9e18888d0b47
sugar-cat7 <study.king7777@gmail.com>
2023-03-17 00:06:15 +0900

    Fix consistency issues

--- web-text-grabber/src/popup.js
+++ web-text-grabber/src/popup.js
@@ -1,13 +1,13 @@
-document.getElementById('extract-text-button').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const grabTextButton = document.getElementById('grabText');
+
+  grabTextButton.addEventListener('click', () => {
+    // Send a message to the content script to get the selected text
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { message: 'extract_text' }, (response) => {
-        const text = response.text;
-        // ここでテキストを利用して何かをする（例：クリップボードにコピー）
-        navigator.clipboard.writeText(text).then(() => {
-          alert('Text content has been copied to the clipboard.');
-        }, (error) => {
-          console.error('Error copying text content to clipboard:', error);
-        });
+      chrome.tabs.sendMessage(tabs[0].id, { type: 'getText' }, (response) => {
+        const textArea = document.getElementById('textArea');
+        textArea.value = response.text;
       });
     });
   });
+});
